8232b9ee86d1a2feb848e7e7f32f384027bbad30
humeragirkar <86532433+Humeragirkar@users.noreply.github.com>
2025-07-12 20:16:22 +0530

    local file

---
+++ infra/terraform/local.tf
@@ -0,0 +1,8 @@
+locals {
+  resource_group_name     = "${var.prefix}-rg"
+  storage_account_name    = substr("${var.prefix}storage", 0, 24)  # Storage account name max length 24
+  cosmos_account_name     = substr("${var.prefix}acct", 0, 44)     # Cosmos account name max length 44
+  cosmos_db_name          = "billing-db"
+  cosmos_container_name   = "records"
+  blob_container_name     = "archived-records"
+}
